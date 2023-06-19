2b1fb7b689382be58bb281e8e761e56b24ede313
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-06-19 15:11:32 +0900

    初期追加

---
+++ InvaderGame/InvaderGame/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace InvaderGame
+{
+    partial class InvaderGame
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SCORE = new System.Windows.Forms.Label();
+            this.SCOREBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SCORE
+            // 
+            this.SCORE.AutoSize = true;
+            this.SCORE.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.SCORE.Location = new System.Drawing.Point(1119, 15);
+            this.SCORE.Name = "SCORE";
+            this.SCORE.Size = new System.Drawing.Size(129, 36);
+            this.SCORE.TabIndex = 0;
+            this.SCORE.Text = "SCORE";
+            this.SCORE.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // SCOREBox
+            // 
+            this.SCOREBox.BackColor = System.Drawing.Color.Black;
+            this.SCOREBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SCOREBox.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.SCOREBox.ForeColor = System.Drawing.Color.White;
+            this.SCOREBox.Location = new System.Drawing.Point(1266, 15);
+            this.SCOREBox.Name = "SCOREBox";
+            this.SCOREBox.Size = new System.Drawing.Size(100, 36);
+            this.SCOREBox.TabIndex = 1;
+            this.SCOREBox.Text = "0";
+            this.SCOREBox.TextChanged += new System.EventHandler(this.SCOREBox_TextChanged);
+            // 
+            // InvaderGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1378, 894);
+            this.Controls.Add(this.SCOREBox);
+            this.Controls.Add(this.SCORE);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "InvaderGame";
+            this.Text = "インベーダーゲーム";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label SCORE;
+        private System.Windows.Forms.TextBox SCOREBox;
+    }
+}
+
---
+++ InvaderGame/InvaderGame/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvaderGame
+{
+    public partial class InvaderGame : Form
+    {
+        public InvaderGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SCOREBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
